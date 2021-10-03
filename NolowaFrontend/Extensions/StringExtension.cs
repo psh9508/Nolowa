75dae174efe6b3a9b3802a4e124392fe42be8630
Masuri <psh0258@gmail.com>
2021-10-04 00:24:32 +0900

    StringExtension 추가

---
+++ NolowaFrontend/Extensions/StringExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NolowaFrontend.Extensions
+{
+    public static class StringExtension
+    {
+        public static bool IsValid(this string src)
+        {
+            return !String.IsNullOrEmpty(src) && !String.IsNullOrWhiteSpace(src);
+        }
+
+        public static bool IsNotVaild(this string src)
+        {
+            return !IsValid(src);
+        }
+    }
+}

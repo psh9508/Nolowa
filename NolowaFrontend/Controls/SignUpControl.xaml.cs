764fbde29d624b12798fae3ba6b5b0a535dd0f45
Masuri <psh0258@gmail.com>
2021-10-29 00:42:47 +0900

    회원가입시 프로필 사진 등록 로직 추가

--- NolowaFrontend/Controls/SignUpControl.xaml.cs
+++ NolowaFrontend/Controls/SignUpControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NolowaFrontend.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -49,6 +50,15 @@ namespace NolowaFrontend.Controls
 
         private void SignupButton_Click(object sender, RoutedEventArgs e)
         {
+            if (ValidateInputText() == false)
+                return;
+
+            if (ValidatePassword() == false)
+            {
+                // Message
+                return;
+            }
+
             Storyboard hideAnimation = (Storyboard)this.Resources[CONFIRM_ANIMATION];
             hideAnimation.Completed += (_, _) => {
                 this.Visibility = Visibility.Hidden;
@@ -56,6 +66,62 @@ namespace NolowaFrontend.Controls
 
             CompleteSignup?.Invoke(sender, e);
             hideAnimation.Begin();
+        }
+
+        private bool ValidateInputText()
+        {
+            if (txtName.InputText.IsNotVaild() 
+                || txtEmail.InputText.IsNotVaild()
+                || txtPassword.InputText.IsNotVaild()
+                || txtPasswordValidation.InputText.IsNotVaild())
+                return false;
+
+            return true;
+        }
+
+        private bool ValidatePassword()
+        {
+            if (txtPassword.InputText != txtPasswordValidation.InputText)
+                return false;
+
+            return true;
+        }
+
+        private void ProfileImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var openFileDialog = new System.Windows.Forms.OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Image Files(*.JPG)|*.JPG|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    var filePath = openFileDialog.FileName;
+                    SetProfileImage(filePath);
+
+                    var fileStream = openFileDialog.OpenFile();
+
+                    //using (StreamReader reader = new StreamReader(fileStream))
+                    //{
+                    //    fileContent = reader.ReadToEnd();
+                    //}
+                }
+            }
+        }
+
+        private void SetProfileImage(string filePath)
+        {          
+            ImageBrush imgBrush = new ImageBrush();
+            BitmapImage btpImg = new BitmapImage();
+
+            btpImg.BeginInit();
+            btpImg.UriSource = new Uri(filePath);
+            btpImg.EndInit();
+            imgBrush.ImageSource = btpImg;
+
+            ProfileImage.Fill = imgBrush;
         }
     }
 }

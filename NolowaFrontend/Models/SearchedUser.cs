a210478189dedf3d85d23a0ee52525f296347664
Masuri <psh0258@gmail.com>
2021-11-09 23:35:22 +0900

    Search Service 작업

---
+++ NolowaFrontend/Models/SearchedUser.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using NolowaFrontend.Models.Images;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NolowaFrontend.Models
+{
+    public class SearchedUser
+    {
+        [JsonProperty("user_id")]
+        public long ID { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("accountId")]
+        public string AccountID { get; set; }
+
+        [JsonProperty("profileImage")]
+        public ProfileImage ProfileImage { get; set; } = new ProfileImage();
+    }
+}
---
+++ NolowaFrontend/Servicies/SearchService.cs
@@ -0,0 +1,26 @@
+﻿using NolowaFrontend.Models;
+using NolowaFrontend.Models.Base;
+using NolowaFrontend.Servicies.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NolowaFrontend.Servicies
+{
+    public interface ISearchService
+    {
+        Task<ResponseBaseEntity<List<SearchedUser>>> Search(string username);
+    }
+
+    public class SearchService : ServiceBase, ISearchService
+    {
+        private const string parentEndPoint = "Search";
+
+        public async Task<ResponseBaseEntity<List<SearchedUser>>> Search(string username)
+        {
+            return await DoGet<List<SearchedUser>>($"{parentEndPoint}/{username}");
+        }
+    }
+}

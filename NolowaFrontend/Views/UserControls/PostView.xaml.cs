89a56a1d6f17eab04998e228f0fb40aa669bf932
Masuri <psh0258@gmail.com>
2022-01-23 10:13:51 +0900

    PostVIew의 Bubbling Event Naming 맞춤

--- NolowaFrontend/Views/UserControls/PostView.xaml.cs
+++ NolowaFrontend/Views/UserControls/PostView.xaml.cs
@@ -33,7 +33,7 @@ namespace NolowaFrontend.Views.UserControls
         /// 프로필 클릭 이벤트를 라우티드이벤트로 만들어서 밖으로 버블링시킴
         /// </summary>
         public static readonly RoutedEvent ClickedProfileImageEvent =
-            EventManager.RegisterRoutedEvent("ClickedProfileImageRouter", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(PostView));
+            EventManager.RegisterRoutedEvent("ClickedProfileImage", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(PostView));
 
         public event RoutedEventHandler ClickedProfileImage
         {

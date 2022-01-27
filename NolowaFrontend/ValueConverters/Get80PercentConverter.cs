356c59ed911386a8de71c921e7ea35d525a863d7
Masuri <psh0258@gmail.com>
2022-01-28 02:17:23 +0900

    프로필 창 매인 크기에서 80프로만 차지하게 뜨도록 변경

---
+++ NolowaFrontend/ValueConverters/Get80PercentConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NolowaFrontend.ValueConverters
+{
+    public class Get80PercentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var test = double.Parse(value.ToString());
+
+            return test * 0.8;
+
+            //var intValue = int.Parse(value.ToString());
+
+            //return intValue * 0.8;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NolowaFrontend/ValueConverters/MinusConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NolowaFrontend.ValueConverters
+{
+    public class MinusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var doubleValue = double.Parse(value.ToString());
+
+            return doubleValue - double.Parse(parameter.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

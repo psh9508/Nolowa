da8fad14b1ec8bae534c8ce9a6fbb5c9caa71e42
Masuri <psh0258@gmail.com>
2022-02-12 14:58:29 +0900

    이미지 버튼 클래스 추가

---
+++ NolowaFrontend/Core/GlobalEventBroadcaster.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NolowaFrontend.Core
+{
+    public class GlobalEventBroadcaster
+    {
+        private static readonly Lazy<GlobalEventBroadcaster> _instance = new Lazy<GlobalEventBroadcaster>(() => new GlobalEventBroadcaster());
+        public static GlobalEventBroadcaster Instance => _instance.Value;
+
+        private GlobalEventBroadcaster()
+        {
+                 
+        }
+
+        public void Register<T> (T eventType)
+        {
+        }
+    }
+}

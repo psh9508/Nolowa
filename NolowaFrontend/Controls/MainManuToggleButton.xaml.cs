481cf51792b107b5854011ff52e8f5c51825951a
Masuri <psh0258@gmail.com>
2022-02-13 16:35:47 +0900

    [메인 버튼 제작] 기본 토글 버튼 제작, 메인에 들어갈 버튼 제작

---
+++ NolowaFrontend/Controls/MainManuToggleButton.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NolowaFrontend.Controls
+{
+    /// <summary>
+    /// MainManuToggleButton.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainManuToggleButton : UserControl
+    {
+        public Uri DefaultImageUri
+        {
+            get { return (Uri)GetValue(DefaultImageUriProperty); }
+            set { SetValue(DefaultImageUriProperty, value); }
+        }
+
+        public static readonly DependencyProperty DefaultImageUriProperty =
+            DependencyProperty.Register("DefaultImageUri", typeof(Uri), typeof(MainManuToggleButton), new PropertyMetadata(null));
+
+        public Uri ClickedImageUri
+        {
+            get { return (Uri)GetValue(ClickedImageUriProperty); }
+            set { SetValue(ClickedImageUriProperty, value); }
+        }
+
+        public static readonly DependencyProperty ClickedImageUriProperty =
+            DependencyProperty.Register("ClickedImageUri", typeof(Uri), typeof(MainManuToggleButton), new PropertyMetadata(null));
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(MainManuToggleButton), new PropertyMetadata(null));
+
+        public MainManuToggleButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NolowaFrontend/Controls/ToggleImageButton.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NolowaFrontend.Controls
+{
+    /// <summary>
+    /// ToggleImageButton.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ToggleImageButton : ToggleButton
+    {
+        public Uri DefaultImageUri
+        {
+            get { return (Uri)GetValue(DefaultImageUriProperty); }
+            set { SetValue(DefaultImageUriProperty, value); }
+        }
+
+        public static readonly DependencyProperty DefaultImageUriProperty =
+            DependencyProperty.Register("DefaultImageUri", typeof(Uri), typeof(ToggleImageButton), new PropertyMetadata(null));
+
+        public Uri ClickedImageUri
+        {
+            get { return (Uri)GetValue(ClickedImageUriProperty); }
+            set { SetValue(ClickedImageUriProperty, value); }
+        }
+
+        public static readonly DependencyProperty ClickedImageUriProperty =
+            DependencyProperty.Register("ClickedImageUri", typeof(Uri), typeof(ToggleImageButton), new PropertyMetadata(null));
+
+        public int ImageWitdh
+        {
+            get { return (int)GetValue(ImageWitdhProperty); }
+            set { SetValue(ImageWitdhProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageWitdhProperty =
+            DependencyProperty.Register("ImageWitdh", typeof(int), typeof(ToggleImageButton), new PropertyMetadata(0));
+        
+        public int ImageHeight
+        {
+            get { return (int)GetValue(ImageHeightProperty); }
+            set { SetValue(ImageHeightProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageHeightProperty =
+            DependencyProperty.Register("ImageHeight", typeof(int), typeof(ToggleImageButton), new PropertyMetadata(0));
+
+        public ToggleImageButton()
+        {
+            InitializeComponent();
+        }
+    }
+}

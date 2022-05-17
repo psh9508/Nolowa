3659f0168a0df0c252085c102c0caeffea5bf6d9
Masuri <psh0258@gmail.com>
2022-05-17 22:57:54 +0900

    ObservableCollection에 AddRange 함수 추가

---
+++ NolowaFrontend/Extensions/ObservableCollectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NolowaFrontend.Extensions
+{
+    public static class ObservableCollectionExtension
+    {
+        public static void AddRange<T>(this ObservableCollection<T> src, IEnumerable<T> list)
+        {
+            foreach (var item in list)
+            {
+                src.Add(item);
+            }
+        }
+    }
+}

58ffe86383c494c5a626bd27fea3accd0ffeeb70
Masuri <psh0258@gmail.com>
2022-05-22 15:45:55 +0900

    프로필 변경 작업 중

---
+++ NolowaFrontend/ViewModels/UpdateProfileVM.cs
@@ -0,0 +1,20 @@
+﻿using NolowaFrontend.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NolowaFrontend.ViewModels
+{
+    public class UpdateProfileVM : ViewModelBase
+    {
+        private bool _isVisible = true;
+
+        public bool IsVisible
+        {
+            get { return _isVisible; }
+            set { _isVisible = value; OnPropertyChanged(); }
+        }
+    }
+}

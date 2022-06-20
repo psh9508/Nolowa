b08dd947ae1d2708c5398a3b4400f024619def28
Masuri <psh0258@gmail.com>
2022-06-20 22:24:34 +0900

    DM 테스트 뷰 작성

---
+++ NolowaFrontend/ViewModels/DirectMessageSendVM.cs
@@ -0,0 +1,14 @@
+﻿using NolowaFrontend.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NolowaFrontend.ViewModels
+{
+    public class DirectMessageSendVM : ViewModelBase
+    {
+
+    }
+}
--- NolowaFrontend/ViewModels/DirectMessageVM.cs
+++ NolowaFrontend/ViewModels/DirectMessageVM.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using NolowaFrontend.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace NolowaFrontend.ViewModels
 {
-    public class DirectMessageVM
+    public class DirectMessageVM : ViewModelBase
     {
+        #region Props
+        private DirectMessageSendVM _directMessageSendVM;
+
+        public DirectMessageSendVM DirectMessageSendVM
+        {
+            get { return _directMessageSendVM; }
+            set { _directMessageSendVM = value; OnPropertyChanged(); }
+        }
+        #endregion
+
+        #region Commands
+        private ICommand _writeDirectMessageCommand;
+
+        public ICommand WriteDirectMessageCommand
+        {
+            get
+            {
+                return GetRelayCommand(ref _writeDirectMessageCommand, _ => {
+                    DirectMessageSendVM = new DirectMessageSendVM();
+                });
+            }
+        }
+        #endregion
     }
 }

b20612dc85bb1b43d07da36a80050afc83dc3e20
Masuri <psh0258@gmail.com>
2022-06-21 22:10:51 +0900

    소켓통신 테스트

--- NolowaFrontend/ViewModels/DirectMessageSendVM.cs
+++ NolowaFrontend/ViewModels/DirectMessageSendVM.cs
@@ -1,14 +1,47 @@
-﻿using NolowaFrontend.ViewModels.Base;
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using NolowaFrontend.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace NolowaFrontend.ViewModels
 {
     public class DirectMessageSendVM : ViewModelBase
     {
+        private readonly HubConnection _hubConnection;
 
+        private ICommand _sendDirectMessageCommand;
+
+        public ICommand SendDirectMessageCommand
+        {
+            get
+            {
+                return GetRelayCommand(ref _sendDirectMessageCommand, async _ =>
+                {
+                    if (_hubConnection.State != HubConnectionState.Connected)
+                        await _hubConnection.StartAsync();
+
+                    await _hubConnection.SendAsync("SendMessage", "clientUser", "clientHello");
+                });
+            }
+        }
+
+        public DirectMessageSendVM()
+        {
+            _hubConnection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:5001/DirectMessage")
+                .WithAutomaticReconnect()
+                .Build();
+
+            _hubConnection.On<string, string>("ReceiveDirectMessage", (user, message) =>
+            {
+
+            });
+
+            _hubConnection.StartAsync();
+        }
     }
 }

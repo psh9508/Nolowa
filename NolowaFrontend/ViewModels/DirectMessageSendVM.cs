dd1e797397b76e6e7e8875fb7aa8073f14b5d52f
Masuri <psh0258@gmail.com>
2022-06-22 00:13:44 +0900

    DM 메시지 주고 받기 테스트 중

--- NolowaFrontend/ViewModels/DirectMessageSendVM.cs
+++ NolowaFrontend/ViewModels/DirectMessageSendVM.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
+using NolowaFrontend.Core;
+using NolowaFrontend.Extensions;
 using NolowaFrontend.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +16,22 @@ namespace NolowaFrontend.ViewModels
     {
         private readonly HubConnection _hubConnection;
 
+        private ObservableCollection<string> _dialog = new ObservableCollection<string>();
+
+        public ObservableCollection<string> Dialog
+        {
+            get { return _dialog; }
+            set { _dialog = value; OnPropertyChanged(); }
+        }
+
+        private string _mseeage = string.Empty;
+
+        public string Message
+        {
+            get { return _mseeage; }
+            set { _mseeage = value; OnPropertyChanged();}
+        }
+
         private ICommand _sendDirectMessageCommand;
 
         public ICommand SendDirectMessageCommand
@@ -21,10 +40,13 @@ namespace NolowaFrontend.ViewModels
             {
                 return GetRelayCommand(ref _sendDirectMessageCommand, async _ =>
                 {
+                    if (Message.IsNotVaild())
+                        return;
+
                     if (_hubConnection.State != HubConnectionState.Connected)
                         await _hubConnection.StartAsync();
 
-                    await _hubConnection.SendAsync("SendMessage", "clientUser", "clientHello");
+                    await _hubConnection.SendAsync("SendMessage", AppConfiguration.LoginUser.Id, Message);
                 });
             }
         }
@@ -36,9 +58,11 @@ namespace NolowaFrontend.ViewModels
                 .WithAutomaticReconnect()
                 .Build();
 
-            _hubConnection.On<string, string>("ReceiveDirectMessage", (user, message) =>
+            _hubConnection.On("ReceiveDirectMessage", (long userId, string message) =>
             {
-
+                System.Windows.Application.Current.Dispatcher.Invoke(() => {
+                    Dialog.Add($"{userId} : {message}");
+                });
             });
 
             _hubConnection.StartAsync();

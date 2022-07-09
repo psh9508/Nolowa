5e9dc0e9c45ac86acc9f05f0ec7f409dfc5789ca
Masuri <psh0258@gmail.com>
2022-07-10 02:07:23 +0900

    MainView에 특수 기능 버튼 제작 완료

---
+++ NolowaFrontend/Controls/Buttons/MainFunctionButton.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NolowaFrontend.Controls.Buttons
+{
+    /// <summary>
+    /// MainFunctionButton.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainFunctionButton : UserControl
+    {
+        public ICommand ClickCommand
+        {
+            get { return (ICommand)GetValue(ClickCommandProperty); }
+            set { SetValue(ClickCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty ClickCommandProperty =
+            DependencyProperty.Register("ClickCommand", typeof(ICommand), typeof(MainFunctionButton), new PropertyMetadata(null));
+
+
+        public string ImageSource
+        {
+            get { return (string)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource", typeof(string), typeof(MainFunctionButton), new PropertyMetadata(string.Empty));
+
+        public int ImageWidth
+        {
+            get { return (int)GetValue(ImageWidthProperty); }
+            set { SetValue(ImageWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageWidthProperty =
+            DependencyProperty.Register("ImageWidth", typeof(int), typeof(MainFunctionButton), new PropertyMetadata(100));
+
+        public MainFunctionButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
